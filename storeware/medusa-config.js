766ed1d82bd0373afca5f181cbb52571ff591d17
Erkan Öz <zo.nakre@gmail.com>
2024-10-01 22:13:49 +0300

    Deploy Medusa Application to Railway Boilerplate

--- storeware/medusa-config.js
+++ storeware/medusa-config.js
@@ -11,6 +11,47 @@ module.exports = defineConfig({
       authCors: process.env.AUTH_CORS,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
+    },
+    workerMode: process.env.MEDUSA_WORKER_MODE,
+    admin: {
+      disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
+    },
+    redisUrl: process.env.REDIS_URL
+  },
+  modules: {
+    [Modules.FILE]: {
+      resolve: '@medusajs/file',
+      options: {
+        providers: [
+          {
+            resolve: '@medusajs/file-local-next',
+            id: 'local',
+            options: {
+              backend_url: `${backendUrl}/static`
+            }
+          }
+        ]
+      }
+    },
+    [Modules.CACHE]: {
+      resolve: "@medusajs/medusa/cache-redis",
+      options: {
+        redisUrl: process.env.REDIS_URL,
+      },
+    },
+    [Modules.EVENT_BUS]: {
+      resolve: "@medusajs/medusa/event-bus-redis",
+      options: {
+        redisUrl: process.env.REDIS_URL,
+      },
+    },
+    [Modules.WORKFLOW_ENGINE]: {
+      resolve: "@medusajs/workflow-engine-redis",
+      options: {
+        redis: {
+          url: process.env.REDIS_URL,
+        },
+      },
+    },
   }
 })
