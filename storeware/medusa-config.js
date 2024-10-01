fc117b9265007079be59d7b91531dc087b5b451d
Erkan Öz <zo.nakre@gmail.com>
2024-10-02 00:12:51 +0300

    fix

--- storeware/medusa-config.js
+++ storeware/medusa-config.js
@@ -15,10 +15,25 @@ module.exports = defineConfig({
     workerMode: process.env.MEDUSA_WORKER_MODE,
     admin: {
       disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
+      backendUrl: process.env.MEDUSA_BACKEND_URL,
     },
     redisUrl: process.env.REDIS_URL
   },
   modules: {
+    [Modules.FILE]: {
+      resolve: '@medusajs/file',
+      options: {
+        providers: [
+          {
+            resolve: '@medusajs/file-local-next',
+            id: 'local',
+            options: {
+              backend_url: `${backendUrl}/static`
+            }
+          }
+        ]
+      }
+    },
     [Modules.CACHE]: {
       resolve: "@medusajs/cache-redis",
       options: {
