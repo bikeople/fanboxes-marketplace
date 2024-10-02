739064dbdb4a9760d297e6ece1cb38da75e73e9f
Erkan Öz <zo.nakre@gmail.com>
2024-10-03 00:19:26 +0300

    fix

--- storeware/medusa-config.js
+++ storeware/medusa-config.js
@@ -13,11 +13,11 @@ module.exports = defineConfig({
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     },
     workerMode: process.env.MEDUSA_WORKER_MODE,
+    redisUrl: process.env.REDIS_URL,
     admin: {
       disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
       backendUrl: process.env.MEDUSA_BACKEND_URL,
-    },
-    redisUrl: process.env.REDIS_URL
+    }
   },
   modules: {
     [Modules.FILE]: {
